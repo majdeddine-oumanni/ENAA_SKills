99127dafd12fe69cdcfdc2a4eb880fab3786904a
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-06-30 17:56:33 +0100

    creating the Repositories of all model entities

---
+++ backend/src/main/java/com/skills/backend/Repository/CompetenceRepository.java
@@ -0,0 +1,7 @@
+package com.skills.backend.Repository;
+
+import com.skills.backend.Model.Competence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompetenceRepository extends JpaRepository<Competence, Long> {
+}
---
+++ backend/src/main/java/com/skills/backend/Repository/SubCompetenceRepository.java
@@ -0,0 +1,7 @@
+package com.skills.backend.Repository;
+
+import com.skills.backend.Model.SubCompetence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCompetenceRepository extends JpaRepository<SubCompetence, Long> {
+}
