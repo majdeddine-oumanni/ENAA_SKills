ad5f1a9284e8060a665c5f152dae63356ad7891d
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-07-01 17:45:01 +0100

    creating CRUD function for the competence Entitie

--- backend/src/main/java/com/skills/backend/Service/CompetenceService.java
+++ backend/src/main/java/com/skills/backend/Service/CompetenceService.java
@@ -1,8 +1,12 @@
 package com.skills.backend.Service;
 
+import com.skills.backend.DTO.CompetenceDTO;
 import com.skills.backend.Mapper.CompetenceMapper;
+import com.skills.backend.Model.Competence;
 import com.skills.backend.Repository.CompetenceRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CompetenceService {
@@ -14,4 +18,26 @@ public class CompetenceService {
         this.repository = repository;
     }
 
+    public CompetenceDTO post(CompetenceDTO dto){
+        Competence competence = mapper.toEntity(dto);
+        Competence savedCompetence = repository.save(competence);
+        return mapper.toDTO(savedCompetence);
+    }
+
+    public CompetenceDTO update(Long id, CompetenceDTO dto){
+        Competence competence = repository.findById(id)
+                .orElseThrow(()->new RuntimeException("competence not found"));
+        competence.setName(dto.getName());
+        CompetenceDTO competenceDTO = mapper.toDTO(repository.save(competence));
+        return competenceDTO;
+    }
+
+    public List<CompetenceDTO> getAll(){
+        List<Competence> competenceList = repository.findAll();
+        return mapper.toDTOs(competenceList);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
