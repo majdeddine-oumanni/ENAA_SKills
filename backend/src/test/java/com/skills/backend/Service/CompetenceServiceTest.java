0ff3f6e7c6b48afba163e18c46146905aef1f615
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-07-04 09:48:31 +0100

    working on tests for the functions in service layer

---
+++ backend/src/test/java/com/skills/backend/Service/CompetenceServiceTest.java
@@ -0,0 +1,22 @@
+package com.skills.backend.Service;
+
+import org.junit.jupiter.api.Test;
+
+class CompetenceServiceTest {
+
+    @Test
+    void post() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ backend/src/test/java/com/skills/backend/Service/SubCompetenceServiceTest.java
@@ -0,0 +1,23 @@
+package com.skills.backend.Service;
+
+import org.junit.jupiter.api.Test;
+
+
+class SubCompetenceServiceTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void updateValidation() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
